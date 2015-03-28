6d09a809395166c850ad465a5c35e6dc197bf69d
abhyudayasrinet <abhyu195@gmail.com>
2015-03-28 18:17:50 +0530

    added commands

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 #Voice Browser
 A voice operated web browser  
+  
 
+#Usage
+Press "Listen" and speak a command  
+
+#Commands
+
+`open <address>`  
+Opens the address you speak. Currently works with ('google','facebook','quora')  
+
+`search <query>`  
+Searches the query on google and speaks out a couple of link titles from the result  
+
+`open link <number>`  
+Choose a link to open from a search query or from the list of bookmarks  
+
+`bookmark page`  
+Bookmarks the page(offline storage not added yet)  
+
+`go back`  
+Move a page backward  
+
+`go forward`  
+Move a page forward  
+
+`read bookmarks`  
+Lists out all the stored bookmarks  
+
+`read page content`  
+Reads the page content(current output only in text not audio)
--- main.py
+++ main.py
@@ -10,143 +10,239 @@
 import pyaudio,os
 import speech_recognition
 from threading import Thread
+from pygoogle import pygoogle
+import time
 import subprocess
 import webkit
 import html2text
 import gtk
 
 speechRecognizer = None
-openWebpage = None
 
 
 
 class Browser():
 
-	def __init__(self):
+    def __init__(self):
 
-		self.init()
+        self.command = ''#'search pizza'
+        self.linkDict = {}
+        self.history = ['']
+        self.historyIndex = 0
+        self.bookmarks = []
+        # self.recognizeCommand()
+        self.init()
+        
 
-	def init(self):
+    def init(self):
 
-		self.mainWindow = gtk.Window()
-		self.mainWindow.set_title("Grizlly Browser")
-		# self.mainWindow.set_opacity(0.8)
-		self.mainWindow.set_position(gtk.WIN_POS_CENTER)
-		self.mainWindow.set_default_size(500,500)
-		# self.mainWindow.maximize()
-		self.mainWindow.connect('destroy' , lambda w: gtk.main_quit())
-		
+        self.mainWindow = gtk.Window()
+        self.mainWindow.set_title("Grizlly Browser")
+        # self.mainWindow.set_opacity(0.8)
+        self.mainWindow.set_position(gtk.WIN_POS_CENTER)
+        self.mainWindow.set_default_size(500,500)
+        # self.mainWindow.maximize()
+        self.mainWindow.connect('destroy' , lambda w: gtk.main_quit())
+        
 
-		self.mainVerticalLayout = gtk.VBox()
-		self.mainWindow.add(self.mainVerticalLayout)
+        self.mainVerticalLayout = gtk.VBox()
+        self.mainWindow.add(self.mainVerticalLayout)
 
-		self.addressBarLayout = gtk.HBox()
-		self.mainVerticalLayout.pack_start(self.addressBarLayout,expand = False, fill = False)
+        self.addressBarLayout = gtk.HBox()
+        self.mainVerticalLayout.pack_start(self.addressBarLayout,expand = False, fill = False)
 
-		self.addressBarText = gtk.Entry()
-		self.addressBarLayout.pack_start(self.addressBarText)
+        self.addressBarText = gtk.Entry()
+        self.addressBarLayout.pack_start(self.addressBarText)
 
-		self.addressBarGoButton = gtk.Button("Go")
-		self.addressBarGoButton.connect('clicked',self.goToAddress)
-		self.addressBarLayout.pack_start(self.addressBarGoButton,expand = False, fill = False)
-		
-		self.addressBarSpeakButton = gtk.Button("Listen")
-		self.addressBarSpeakButton.connect('clicked',self.ListenAddress)	
-		self.addressBarLayout.pack_start(self.addressBarSpeakButton,expand = False, fill = False)
-		
-		self.browserWindow = gtk.ScrolledWindow()
-		self.mainVerticalLayout.pack_start(self.browserWindow)
+        self.addressBarGoButton = gtk.Button("Go")
+        self.addressBarGoButton.connect('clicked',self.goButtonCB)
+        self.addressBarLayout.pack_start(self.addressBarGoButton,expand = False, fill = False)
+        
+        self.addressBarSpeakButton = gtk.Button("Speak")
+        self.addressBarSpeakButton.connect('clicked',self.listenerThread)    
+        self.addressBarLayout.pack_start(self.addressBarSpeakButton,expand = False, fill = False)
+        
+        self.browserWindow = gtk.ScrolledWindow()
+        self.mainVerticalLayout.pack_start(self.browserWindow)
 
-		self.browserWebView = webkit.WebView()
-		self.browserWindow.add(self.browserWebView)
-		self.browserWebView.connect("load-finished", self.getPageContent)
+        self.browserWebView = webkit.WebView()
+        self.browserWindow.add(self.browserWebView)
+        self.browserWebView.connect("load-finished", self.pageLoaded)
 
-		self.mainWindow.show_all()
+        self.mainWindow.show_all()
 
-		gtk.main()
-
-	def speechListener(self,source):
-
-		global speechRecognizer,openWebpage
-		print("listening...")
-		# audio = speechRecognizer.listen(source,3)
-		audio = speechRecognizer.record(source,3)
-		print("done listening!")
-		try:
-			user = speechRecognizer.recognize(audio)
-			print('You said '+user)
-			openWebpage = user
-			stream = subprocess.Popen(['espeak',str(user)])
-			# out,err = stream.communicate()
-			# print(out,err)
-		except LookupError:
-			stream = subprocess.Popen(['espeak','please say that again'])
-			print("Can't understand")
-		
-
-	def goToURL(self,url):
-		if(url == "google"):
-			self.browserWebView.open("http://www.google.com")
-		elif(url == "facebook"):
-			self.browserWebView.open("http://www.facebook.com")
-		elif(url == "quora"):
-			self.browserWebView.open("http://www.quora.com")
-		else:
-			self.browserWebView.open(url)
-
-		
-	def getPageContent(self,webview, frame):
-		stream = subprocess.Popen(['espeak','page loaded'])
-		x = self.browserWebView.get_main_frame().get_data_source().get_data()
-		print(html2text.html2text(x))
+        gtk.main()
 
 
-	def goToAddress(self, widget):
-		add = self.addressBarText.get_text()
-		self.goToURL(add)
+    def goButtonCB(self, widget):
+        text = self.addressBarText.get_text()
+        if(text.startswith('http://') or text.startswith('https://') ):
+            self.goToURL(text)
+        else:
+            text = 'https://'+text
+            self.addressBarText.set_text(text)
+            self.goToURL(text)
 
 
-	def ListenAddress(self,widget):
+    def speechListener(self,source):
 
-		global speechRecognizer,openWebpage
-		speechRecognizer = speech_recognition.Recognizer()
-		speechRecognizer.energy_threshold = 1000
-		speechRecognizer.pause_threshold = 0.5
-		with speech_recognition.Microphone() as source:
-			listenerThread = Thread(target = self.speechListener, args = (source, ))
-			listenerThread.start()
-			listenerThread.join()
-			print("finished listening")
-		
-		self.goToURL(openWebpage)
+        global speechRecognizer,openWebpage
+        print("listening...")
+        # audio = speechRecognizer.listen(source,3)
+        audio = speechRecognizer.record(source,3)
+        print("done listening!")
+        try:
+            self.command = speechRecognizer.recognize(audio)
+            print('You said '+self.command)
+            stream = subprocess.Popen(['espeak',str(self.command)])
+            stream.wait()
+            # out,err = stream.communicate()
+            # print(out,err)
+        except LookupError:
+            stream = subprocess.Popen(['espeak','please say that again'])
+            self.command = ''
+            print("Can't understand")
+
+        
+    def listenerThread(self,widget):
+
+        global speechRecognizer
+        speechRecognizer = speech_recognition.Recognizer()
+        speechRecognizer.energy_threshold = 2000
+        speechRecognizer.pause_threshold = 0.5
+        with speech_recognition.Microphone() as source:
+            listenerThread = Thread(target = self.speechListener, args = (source, ))
+            listenerThread.start()
+            listenerThread.join()
+            print("finished listening")
+        
+        self.recognizeCommand()
+
+    def recognizeCommand(self):
+
+        self.command = self.command.encode('ascii')
+        words = self.command.split() #split by spaces
+        print(words)
+
+        if(words[0] == "open"):
+            if(words[1] == "link"):
+                self.goToURL(self.linkDict[self.getNumber(words[2])])
+            else:
+                self.goToURL(words[1])
+
+        elif(words[0] == "search"):
+            self.searchGoogle(words[1:])
+
+        #todo option to add tag for bookmark
+        elif(self.command == "bookmark page"):
+            if(self.history[self.historyIndex] not in self.bookmarks):
+                self.bookmarks.append(self.history[self.historyIndex])
+                stream = subprocess.Popen(['espeak','bookmark added'])
+                stream.wait()
+
+        elif(self.command == "go back"):
+            self.historyIndex -= 1
+            self.goToURL(self.history[self.historyIndex], True)
+
+        elif(self.command == "go forward"):
+            self.historyIndex += 1
+            self.goToURL(self.history[self.historyIndex], True)            
+
+        elif(self.command == "read bookmarks"):
+            self.linkDict = {}       
+            i = 1   
+            for bookmark in self.bookmarks:
+                stream = subprocess.Popen(['espeak','Link '+str(i)])
+                stream.wait()
+                stream = subprocess.Popen(['espeak',str(bookmark)])
+                stream.wait()
+                self.linkDict[i] = bookmark
+            print("linkDict:",self.linkDict)
+
+        elif(self.command == "read page content"):
+            self.getPageContent()
 
 
-if __name__ == "__main__":	
+    def goToURL(self, url, fowback = False):
 
-	browser = Browser()
+        print("opening url:",url)
+        if(url == "google"):
+            self.browserWebView.open("http://www.google.com")
+        elif(url == "facebook"):
+            self.browserWebView.open("http://www.facebook.com")
+        elif(url == "quora"):
+            self.browserWebView.open("http://www.quora.com")
+        else:
+            self.browserWebView.open(url)
 
-	
+        if(not fowback):
+            del self.history[self.historyIndex+1:]
+            self.history.append(url)
+            self.historyIndex = len(self.history) - 1
 
-	
+    
+    def searchGoogle(self, query):
 
-	
+        query = ' '.join(query)
+        g = pygoogle(query,1)
+        searchDict = g.search()
+        i = 1
+        for key in searchDict:
+            print(key,":",str(searchDict[key]))
+            #uncomment to speak out links in order
+            # stream = subprocess.Popen(['espeak','Link '+str(i)])
+            # stream.wait()
+            # stream = subprocess.Popen(['espeak',str(key).encode('utf-8')])
+            # stream.wait()
+            self.linkDict[i] = searchDict[key]
+            i += 1
+        print("linkdict:",self.linkDict)
 
-	
+
+    def getNumber(self, word):
+        print("getNumber : ",word)
+        if(type(word) == int):
+            return word
+        if(word == "zero"):
+            return 0
+        if(word == "one"):
+            return 1
+        if(word == "two"):
+            return 2
+        if(word == "three"):
+            return 3
+        if(word == "four"):
+            return 4
+        if(word == "five"):
+            return 5
+        if(word == "six"):
+            return 6
+        if(word == "seven"):
+            return 7
+        if(word == "eight"):
+            return 8
+        if(word == "nine"):
+            return 9
+        if(word == "ten"):
+            return 10
+
+    def pageLoaded(self,webview, frame):
+        stream = subprocess.Popen(['espeak','page loaded'])
+
+    def getPageContent(self):
+        x = self.browserWebView.get_main_frame().get_data_source().get_data()
+        h = html2text.HTML2Text()
+        h.ignore_links = True
+        # print(html2text.html2text(x))
+        print(h.handle(x))
 
 
+    
+
+    
 
 
-#WORKING
-#Recognize from audio file
-# import speech_recognition as speech_recognition
-# r = speech_recognition.Recognizer()
-# with speech_recognition.WavFile("opengoogle.wav") as source:        # use "test.wav" as the audio source
-#     audio = r.record(source)                        # extract audio data from the file
+if __name__ == "__main__":  
 
-# try:
-#     list = r.recognize(audio,True)                  # generate a list of possible transcriptions
-#     print("Possible transcriptions:")
-#     for prediction in list:
-#         print(" " + prediction["text"] + " (" + str(prediction["confidence"]*100) + "%)")
-# except LookupError:                                 # speech is unintelligible
-#     print("Could not understand audio")
+    browser = Browser()
+    
